cfc4330a37910a4ef408804fa270c2069a7a9581
Ioannis Kokkinis <info@upg.gr>
2018-01-28 01:19:41 +0200

    Updated stellar_daemon.py.

--- stellar_daemon.py
+++ stellar_daemon.py
@@ -1,8 +1,17 @@
+import threading
 from stellar_base.address import Address
 publickey = 'GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH'
 address = Address(address=publickey,network='testnet') # address = Address(address=publickey,network='public') for livenet
-address.get() # get the updated information
-print address.balances
+def printit():
+  threading.Timer(5.0, printit).start()
+#  print "Hello, World!"
+  address.get() # get the updated information
+  print address.balances
+printit()
+
+
+
+
 #print "balances: " + address.balances
 #print "sequence: " + address.sequence
 #print "flags: " + address.flags
