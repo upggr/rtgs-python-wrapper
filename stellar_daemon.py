fd0117c596edc74ea343fa85553bfaf7415014a2
Ioannis Kokkinis <info@upg.gr>
2018-01-28 01:58:23 +0200

    Updated stellar_daemon.py.

--- stellar_daemon.py
+++ stellar_daemon.py
@@ -1,48 +1,19 @@
-from threading import Timer
-
-class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer     = None
-        self.function   = function
-        self.interval   = interval
-        self.args       = args
-        self.kwargs     = kwargs
-        self.is_running = False
-        self.start()
-
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
-
-        
-from time import sleep
+import schedule
+import time
 from stellar_base.address import Address
 publickey = 'GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH'
 address = Address(address=publickey,network='testnet') # address = Address(address=publickey,network='public') for livenet
 
+def job():
+    address.get() # get the updated information
+    balance = str(address.balances)
+    print "balance: " + balance
 
-def hello(name):
-    print "Hello %s!" % name
+schedule.every(2).seconds.do(job)
+while 1:
+   schedule.run_pending()
+   time.sleep(1)
 
-print "starting..."
-rt = RepeatedTimer(1, hello, "World") # it auto-starts, no need of rt.start()
-try:
-      address.get() # get the updated information
-      balance = str(address.balances)
-      print "balance: " + balance
-finally:
-    rt.stop() # better in a try/finally block to make sure the program ends!
 
 
 
