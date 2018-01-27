d0520758718d8da4b87a2ac8f977811b4144e3dc
Ioannis Kokkinis <info@upg.gr>
2018-01-28 01:39:01 +0200

    Updated stellar_daemon.py.

--- stellar_daemon.py
+++ stellar_daemon.py
@@ -2,16 +2,19 @@ import threading
 from stellar_base.address import Address
 publickey = 'GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH'
 address = Address(address=publickey,network='testnet') # address = Address(address=publickey,network='public') for livenet
-def printit():
-  threading.Timer(2.0, printit).start()
+global new_balance
+global old_balance
+def worker():
+  threading.Timer(2.0, worker).start()
 #  print "Hello, World!"
+
   address.get() # get the updated information
   new_balance = str(address.balances)
 #  print address.balances
   print "old balance: " + old_balance
   print "new balance: " + new_balance
   old_balance = str(address.balances)
-printit()
+worker()
 
 
 
