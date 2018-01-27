91a1e548bbaf3ecfc47e42bd1307ebc76382ef2d
Ioannis Kokkinis <info@upg.gr>
2018-01-28 00:43:32 +0200

    Updated stellar_daemon.py.

--- stellar_daemon.py
+++ stellar_daemon.py
@@ -2,9 +2,10 @@ from stellar_base.address import Address
 publickey = 'GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH'
 address = Address(address=publickey,network='testnet') # address = Address(address=publickey,network='public') for livenet
 address.get() # get the updated information
-print "balances: " + address.balances
-print "sequence: " + address.sequence
-print "flags: " + address.flags
-print "signers: " + address.signers
-print "data: " + address.data
+print address.balances
+#print "balances: " + address.balances
+#print "sequence: " + address.sequence
+#print "flags: " + address.flags
+#print "signers: " + address.signers
+#print "data: " + address.data
 # url = 'http://195.201.17.80:8000/accounts/GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH/payments'
