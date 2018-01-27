6311ef1fea7ed61c2b07a4894bd960a0096696e9
Ioannis Kokkinis <info@upg.gr>
2018-01-28 00:38:03 +0200

    Updated stellar_daemon.py.

--- stellar_daemon.py
+++ stellar_daemon.py
@@ -1,20 +1,6 @@
-import json
-import pprint
-import sseclient
+from stellar_base.address import Address
+publickey = 'GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH'
+address = Address(address=publickey,network='testnet') # address = Address(address=publickey,network='public') for livenet
+address.get() # get the updated information
 
-def with_urllib3(url):
-    """Get a streaming response for the given event feed using urllib3."""
-    import urllib3
-    http = urllib3.PoolManager()
-    return http.request('GET', url, preload_content=False)
-
-def with_requests(url):
-    """Get a streaming response for the given event feed using requests."""
-    import requests
-    return requests.get(url, stream=True)
-
-url = 'http://195.201.17.80:8000/accounts/GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH/payments'
-response = with_urllib3(url)  # or with_requests(url)
-client = sseclient.SSEClient(response)
-for event in client.events():
-    pprint.pprint(json.loads(event.data))
+# url = 'http://195.201.17.80:8000/accounts/GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH/payments'
