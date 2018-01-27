f15ee9e9e8bcdf26f20d2f4cf93a87f49e77abe6
Ioannis Kokkinis <info@upg.gr>
2018-01-27 21:18:49 +0200

    Updated stream_payments.js.

--- stellar-testnet/stream_payments.js
+++ stellar-testnet/stream_payments.js
@@ -1,5 +1,5 @@
 var EventSource = require('eventsource');
-var es = new EventSource('http://195.201.17.80:8000//accounts/GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH/payments');
+var es = new EventSource('http://195.201.17.80:8000/accounts/GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH/payments');
 es.onmessage = function(message) {
   var result = message.data ? JSON.parse(message.data) : message;
   console.log('New payment:');
