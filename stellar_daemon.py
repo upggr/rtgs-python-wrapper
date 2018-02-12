572bebd7493566015fc37e386985acfda8e16ce1
Ioannis Kokkinis <info@upg.gr>
2018-02-12 21:20:33 +0200

    Updated stellar_daemon.py.

--- stellar_daemon.py
+++ stellar_daemon.py
@@ -31,6 +31,6 @@ def startchecks():
     looparray(watchlist)
 
 startchecks();
-
+printword('check')
 
 # url = 'http://195.201.17.80:8000/accounts/GCQ2WFN74IOHNRCKS5HWQGM73QVOCYRX5VN53FFQREJDHJ7BM5U7PJCH/payments'
