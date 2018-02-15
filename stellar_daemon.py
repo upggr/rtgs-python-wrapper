fa0946e360b0a2971f55efe49e4afacbcd32ec8c
Ioannis Kokkinis <info@upg.gr>
2018-02-15 15:56:41 +0200

    Updated stellar_daemon.py.

--- stellar_daemon.py
+++ stellar_daemon.py
@@ -64,7 +64,7 @@ def processwebhooks():
 
 def callwebhook(wallet,balance):
     cursor = db.cursor()
-    cursor.execute('''UPDATE webhook_operations SET webhook_notified = ? WHERE pkey = ? AND balance = ?''', ('ok',wallet,balance))
+    cursor.execute('''UPDATE webhook_operations SET webhook_notified = ? WHERE pkey = ? AND balance = ?''', ("ok",wallet,balance))
     print('webhook call logged in the database')
     db.commit()
 
